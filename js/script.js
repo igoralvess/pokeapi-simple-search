f3f81dc119f683abea5832a5ca891fbfc65b9507
Igor Alves <igor@novaxs.com.br>
2016-11-03 17:55:44 -0200

    att arquivos

--- js/script.js
+++ js/script.js
@@ -1,7 +1,11 @@
-
 function habilitaBtn(){ 
-	var texto = document.getElementById("campo").value.replace(" ","");
-	var cont = texto.length;
+
+	var campo = document.getElementById("campo").value;
+
+	while(campo.indexOf(" ") != -1){
+ 		campo = campo.replace(" ", "");
+ 	}
+ 	var cont = campo.length;
 
 	if(cont >= 3 ){
 
@@ -9,5 +13,19 @@ function habilitaBtn(){
 	} else{
 		document.getElementById("botao").disabled = true;
 	}
+}
 
-} 
+function callApi(){
+	var nome = document.getElementById("campo").value;
+	var xhr = new XMLHttpRequest();
+	xhr.open("GET", "http://pokeapi.co/api/v2/pokemon/charmander", true);
+	xhr.send(null);
+	xhr.onreadystatechange = function () 
+	{
+		
+	    if (xhr.readyState === 4)
+	    {
+	        //console.log(xhr.response);
+	    }
+	}
+}
